e5da441f7accf9695c8f4205995d76d704ded321
Stas <sieliseev@gmail.com>
2017-06-23 18:23:06 +0300

    4 lvl

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        String s = "Stas";
+    }
+}
