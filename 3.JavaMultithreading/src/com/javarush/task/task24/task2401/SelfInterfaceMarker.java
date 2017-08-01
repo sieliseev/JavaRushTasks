0ea3cebd3986c46f74ba1539d6876d962121634b
Stas <sieliseev@gmail.com>
2017-08-01 10:27:13 +0300

    24 lvl

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Stas on 01.08.2017.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Stas on 01.08.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void print() {
+        System.out.println();
+    }
+
+    public void print2() {
+        System.out.println();
+    }
+}
