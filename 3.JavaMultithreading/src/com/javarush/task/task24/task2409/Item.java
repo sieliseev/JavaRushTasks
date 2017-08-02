648cc50b02735c4ddc392688b154888df98ab2c0
Stas <sieliseev@gmail.com>
2017-08-02 14:59:55 +0300

    24 lvl

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Stas on 01.08.2017.
+ */
+public interface Item {
+    int getId();
+
+    double getPrice();
+
+    String getTM();
+}
